dbf4a596f82d297ba33700d4020cda73d99d7872
Shardul Joshi <yo.shardul@gmail.com>
2022-08-04 13:54:39 +0530

    Made leftover updates

--- middleware.js
+++ middleware.js
@@ -1,54 +1,53 @@
-const { campgroundSchema, reviewSchema } = require('./schemas')
-const ExpressError = require('./utils/ExpressError')
-const Campground = require('./models/campground')
-const Review = require('./models/review')
+const { campgroundSchema, reviewSchema } = require("./schemas");
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.goBack = req.originalUrl
-        req.flash('error', 'You have to be logged in')
-        return res.redirect('/login')
-    }
-    next()
-}
+  if (!req.isAuthenticated()) {
+    req.session.goBack = req.originalUrl;
+    req.flash("error", "You have to be logged in");
+    return res.redirect("/login");
+  }
+  next();
+};
 
 module.exports.validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-
-}
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 module.exports.isAuthor = async (req, res, next) => {
-    const { id } = req.params
-    const foundCamp = await Campground.findById(id)
-    if (!foundCamp.author.equals(req.user._id)) {
-        req.flash('error', "You don't have the persmission to do that!")
-        return res.redirect(`/campgrounds/${foundCamp._id}`)
-    } next()
-}
-
+  const { id } = req.params;
+  const foundCamp = await Campground.findById(id);
+  if (!foundCamp.author.equals(req.user._id)) {
+    req.flash("error", "You don't have the persmission to do that!");
+    return res.redirect(`/campgrounds/${foundCamp._id}`);
+  }
+  next();
+};
 
 module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const { id, reviewId } = req.params
-    const foundReview = await Review.findById(reviewId)
-    if (!foundReview.author.equals(req.user._id)) {
-        req.flash('error', "You don't have the persmission to do that!")
-        return res.redirect(`/campgrounds/${id}`)
-    } next()
-}
+  const { id, reviewId } = req.params;
+  const foundReview = await Review.findById(reviewId);
+  if (!foundReview.author.equals(req.user._id)) {
+    req.flash("error", "You don't have the persmission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
